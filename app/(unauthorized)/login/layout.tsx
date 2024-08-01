f85415247bfdabdf9097eca771def6d70f88a596
jakeaturner <jturner@cosmistack.com>
2024-07-31 17:31:42 -0700

    fix: [wip] auth

--- app/(unauthorized)/login/layout.tsx
+++ app/(unauthorized)/login/layout.tsx
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "../../globals.css";
 import "bootstrap/dist/css/bootstrap.css";
 import classNames from "classnames";
+import IFrameResizer from "@/components/IFrameResizer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,7 @@ export default function RootLayout({
       <body
         className={classNames(inter.className, "container mt-4 default-layout")}
       >
+        <IFrameResizer />
         <div className="tw-flex tw-flex-row">
           <div className="tw-flex tw-ml-6 !tw-w-full">{children}</div>
         </div>
