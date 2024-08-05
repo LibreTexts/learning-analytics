af80b752381c5b7ca346159ad60bb6b133f31938
Jake Turner <jturner@cosmistack.com>
2024-08-05 13:12:15 -0700

    fix(Auth): cleanup and finalize auth

--- components/AuthEventListener.tsx
+++ components/AuthEventListener.tsx
@@ -15,19 +15,19 @@ const AuthEventListener = ({ debug = false, originMatch }: { debug?: boolean, or
   }, []);
 
   async function handleMessage(event: MessageEvent) {
-    if (originMatch && event.origin !== originMatch) return;
-    if (event.data.type === "AUTH_TOKEN") {
-      const token = event.data.token;
-      {
-        debug && console.log(token);
-      }
-      const success = await adaptLogin(token);
-      if (success) {
-        {
-          debug && console.log("Login successful, redirecting to home page");
-        }
-        return router.push("/");
-      }
+    if (originMatch && event.origin !== originMatch) {
+      console.error(`Origin ${event.origin} does not match expected value: ${originMatch}`);
+    }
+
+    if (event.data.type !== "AUTH_TOKEN") return
+    const token = event.data.token;
+
+    if (debug) console.log("Received token from parent window: ", token);
+
+    const success = await adaptLogin(token);
+    if (success) {
+      if (debug) console.log("Login successful, redirecting to home page");
+      return router.push("/");
     }
   }
 
